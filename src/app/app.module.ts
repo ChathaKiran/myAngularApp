0db4d67cf47283550ae59cf85720923e761ddfd7
Kiran Chatha <kchatha@eprod.com>
2019-09-17 13:50:45 -0500

    Added Port number for browser Url; Added comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent], //List all the components belonging to this module
+  imports: [BrowserModule], //for External Modules
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //This is the startup component of the application. Should have the selectos we use in index.html
 })
-export class AppModule { }
+export class AppModule { } //When the compiler finds a directive in template, it looks at the AppModule to check if the component is defined under it.
